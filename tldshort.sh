7ba1c03bd924041cd4d86dd4ddf2bf8388a3181d
Delonte Stewart <dstewart@tiggee.com>
2019-09-03 09:13:53 -0400

    commit

--- tldshort.sh
+++ tldshort.sh
@@ -5,4 +5,5 @@ tld=`echo $1 | awk -F '.' '{print $2}'`
 foo=`host -t ns $tld | grep 'name server' | head -n 1 | awk '{print $4}'`
 dig=`dig $domain @$foo | grep 'IN\sNS' | head -n 1 | awk '{print $5}'`
 record=`dig $domain @$dig +short`
-echo $record
+thanos= `echo $record | sed -e 's#\([[:digit:]]\{3\}\).\([[:digit:]]\{3\}\).\([[:digit:]]\{3\}\).*#\1.\2.#'`
+echo $thanos
