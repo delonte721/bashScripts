1154c3ad854204bce0ea982cae19da6ddb4fe944
Delonte Stewart <dstewart@tiggee.com>
2019-09-06 05:55:34 -0400

    commit

--- curlshort.sh
+++ curlshort.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #curlshort 
+URL=$1
+curl=`curl -I $URL 2>/dev/null | head -n 1`
+location=`curl -I $URL 2>/dev/null | head -n 5`
+if  [[ `echo $curl | grep -E 301\|302` ]]; 
+then 
+    echo $location
+else 
+    echo $curl
+fi
