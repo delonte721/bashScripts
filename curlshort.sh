01572c46a05a222c64176e7ebfed5e38bb8a218f
Delonte Stewart <dstewart@tiggee.com>
2019-09-13 19:33:31 -0400

    commit

--- curlshort.sh
+++ curlshort.sh
@@ -5,12 +5,6 @@ curl=`curl -I $URL 2>/dev/null | head -n 1`
 redirect=`curl -I -Ls -o /dev/null -w %{url_effective} $URL`
 location=`curl -I $redirect 2>/dev/null | head -n 1`
 chain=`curl --head --silent --location --output /dev/null --write-out "It took %{num_redirects} redirect/redirects to get %{url_effective}\n" $URL`
-
-while [ -n "${URL}" ]
-do
-    echo $URL
-    URL=$(curl -sw "\n\n%{redirect_url}" "${URL}" | tail -n 1)
-done
    
    if  [[ `echo $curl | grep -E 301\|302` ]]; 
    then 
@@ -19,6 +13,12 @@ done
       echo $curl
    fi
    
+    while [ -n "${URL}" ]
+do
+    echo $URL
+    URL=$(curl -sw "\n\n%{redirect_url}" "${URL}" | tail -n 1)
+done
+
    if [[ $2 == *"trace"* ]]; 
    then
       echo $chain
