5b4563ecc4b9be3532ef8b298cedebf6ec3712e4
Delonte Stewart <43830595+delonte721@users.noreply.github.com>
2020-05-21 14:49:25 -0400

    Update tldshort.sh

--- tldshort.sh
+++ tldshort.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #TLDShort
+#This script will give out the IP address and with thanos snap will delete half of the IP address. 
 domain=$1
 tld=`echo $1 | awk -F '.' '{print $2}'`
 foo=`host -t ns $tld | grep 'name server' | head -n 1 | awk '{print $4}'`
