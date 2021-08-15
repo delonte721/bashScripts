b1f622e20d5488bf174fe8af2bb5f0c85e34ff99
Delonte Stewart <lonte721@gmail.com>
2021-08-15 15:02:46 -0400

    commit

--- dskey.sh
+++ dskey.sh
@@ -3,9 +3,16 @@
 
 if [ $# -lt 1 ]; then
 	echo ""
-	echo "This script will show the FQDN's DS record information
+	echo "This script will show the DS record of a single FQDN"
+	echo "--- Incorrect Syntax ----"
 	echo "Single FQDN"
-	echo "Usage: dskey.sh <domain>"
+    echo "Usage: dsrecord <domain> / dsrecord <domain> nospace"
+    echo "Example: dsrecord constellix.com"
+    echo "Example: dsrecord constellix.com nospace"
+	echo "File List of FQDNs"
+	echo "Usage: dsrecord <file_name> / dsrecord <file_name>"
+	echo "Example: dsrecord testsslexpire.txt"
+	echo "Example: dsrecord testsslexpire.txt nospace"
 	echo ""
 	exit
 fi
