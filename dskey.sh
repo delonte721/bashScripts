9a029c458c363c9d95827d7261aaeee6d8fbe326
Delonte Stewart <lonte721@gmail.com>
2021-08-15 14:01:10 -0400

    commit

--- dskey.sh
+++ dskey.sh
@@ -3,6 +3,9 @@
 
 if [ $# -lt 1 ]; then
 	echo ""
-	echo "This scripti will verify and show the URL's SSL cert information. Adding dates will only show the host, start, and expire dates of SSL cert"
-	echo "--- Incorrect Syntax ----"
+	echo "This script will show the FQDN's DS record information
+	echo "Single FQDN"
+	echo "Usage: dskey.sh <domain>"
+	echo ""
+	exit
     
