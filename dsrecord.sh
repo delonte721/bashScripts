17b6da61d7539b094278807efc1094819d22535e
Delonte Stewart <lonte721@gmail.com>
2021-08-18 09:34:05 -0400

    commit

--- dsrecord.sh
+++ dsrecord.sh
@@ -16,52 +16,42 @@ if [ $# -lt 1 ]; then
 	echo ""
 	exit
 fi
-nameserver_1="@ns1.dnsmadeeasy.com"
-nameserver_2="@ns10.dnsmadeeasy.com"
-dsrecord_1=`dig +short $nameserver_1 $1 in cds`
-dsrecord_2=`dig +short $nameserver_2 $1 in cds`
-keyID=`echo "$dsrecord_1" | awk '{print $1}'`
-algorithm=`echo "$dsrecord_1" | awk '{print $2}'`
-digest_type=`echo "$dsrecord_1" | awk '{print $3}'`
-digest=`echo "$dsrecord_1" | awk '{print $4}'`
-digest_2=`echo "$dsrecord_1" | awk '{print $5}'`
-keyID2=`echo "$dsrecord_2" | awk '{print $1}'`
-algorithm2=`echo "$dsrecord_2" | awk '{print $2}'`
-digest_type2=`echo "$dsrecord_2" | awk '{print $3}'`
-digest2=`echo "$dsrecord_2" | awk '{print $4}'`
-digest22=`echo "$dsrecord_2" | awk '{print $5}'`
-
-if [ -f $1 ]; then
-	for x in `grep -v ^# $1 | awk '{print $1}'`; do
-		echo "DNSSEC has been enabled for $x:"
-		if [[ $dsrecord_1 ]]; then
-			echo ""
-			echo "KeyID:$keyID"
-			echo "Algorithm:$algorithm"
-			echo "Digest Type:$digest_type"
-			echo "Digest:$digest$digest_2"
-			echo ""
-		else
-			echo ""
-			echo "KeyID:$keyID2"
-			echo "Algorithm:$algorithm2"
-			echo "Digest Type:$digest_type2"
-			echo "Digest:$digest2$digest22"
-			echo ""
-		fi
-	done
-else
+getDSrecord() {
+	nameserver_1="@ns1.dnsmadeeasy.com"
+	nameserver_2="@ns10.dnsmadeeasy.com"
+	dsrecord_1=`dig +short $nameserver_1 $1 in cds`
+	dsrecord_2=`dig +short $nameserver_2 $1 in cds`
+	keyID=`echo "$dsrecord_1" | awk '{print $1}'`
+	algorithm=`echo "$dsrecord_1" | awk '{print $2}'`
+	digest_type=`echo "$dsrecord_1" | awk '{print $3}'`
+	digest=`echo "$dsrecord_1" | awk '{print $4}'`
+	digest_2=`echo "$dsrecord_1" | awk '{print $5}'`
+	keyID2=`echo "$dsrecord_2" | awk '{print $1}'`
+	algorithm2=`echo "$dsrecord_2" | awk '{print $2}'`
+	digest_type2=`echo "$dsrecord_2" | awk '{print $3}'`
+	digest2=`echo "$dsrecord_2" | awk '{print $4}'`
+	digest22=`echo "$dsrecord_2" | awk '{print $5}'`
+	
 	echo "DNSSEC has been enabled for $1:"	
-if [[ $dsrecord_1 ]]; then
-	echo "KeyID:$keyID"
-	echo "Algorithm:$algorithm"
-	echo "Digest Type:$digest_type"
-	echo "Digest:$digest$digest_2"
-	echo ""
-else
-	echo "KeyID:$keyID2"
-	echo "Algorithm:$algorithm2"
-	echo "Digest Type:$digest_type2"
-	echo "Digest:$digest2$digest22"
-	echo ""
+	if [[ $dsrecord_1 ]]; then
+		echo "KeyID:$keyID"
+		echo "Algorithm:$algorithm"
+		echo "Digest Type:$digest_type"
+		echo "Digest:$digest$digest_2"
+		echo ""
+	else
+		echo "KeyID:$keyID2"
+		echo "Algorithm:$algorithm2"
+		echo "Digest Type:$digest_type2"
+		echo "Digest:$digest2$digest22"
+		echo ""
+	fi
+}
+if [ -f $1 ]; then
+    file=$1
+    while read FQDN; do
+        echo "DNSSEC has been enabled for $FQDN"
+		getDSrecord
+        echo
+    done < $1
 fi
